32bf04db9c584e4ae887cf1d9adedf8d6b3c45ab
Harrison <harrisonhemstreet@gmail.com>
2020-12-31 15:10:11 -0700

    added getTokenFromUrl

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Login from './Login.js';
+import {getTokenFromUrl} from './spotify'
 
 function App() {
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log(token);
+  }, [])
   return (
     <div className="App">
       {/* Spotify Logo */}
--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,6 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
-const redirectUri = 'http:localhost:2000/';
+const redirectUri = 'http://localhost:3001/';
 
 const clientId = '582d00c402784566bbb13e407c986753';
 
@@ -12,4 +12,17 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
-export const loginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            var parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1])
+
+            return initial;
+        }, {});
+
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
