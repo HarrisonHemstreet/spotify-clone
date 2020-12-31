112ebb64f05adf6c71bbf8a410b853d946e06c7e
Harrison <harrisonhemstreet@gmail.com>
2020-12-31 15:18:32 -0700

    added useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Login from './Login.js';
 import {getTokenFromUrl} from './spotify'
 
 function App() {
+  const [token, setToken] = useState(null);
   useEffect(() => {
-    const token = getTokenFromUrl();
-    console.log(token);
-  }, [])
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+    
+  }, []);
+
   return (
     <div className="App">
       {/* Spotify Logo */}
