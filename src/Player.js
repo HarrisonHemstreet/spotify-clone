e93a0498191a2d4d9fe061cbdd3504b123ad3765
Harrison <harrisonhemstreet@gmail.com>
2021-01-02 17:24:29 -0700

    added stuff to player and css

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Player.css';
 import Sidebar from './Sidebar'
+import Body from './Body';
+import Footer from './Footer';
 
 function Player() {
     return (
@@ -8,11 +10,12 @@ function Player() {
             {/* sidebar/body */}
             <div className='player__sidebar'>
                 <Sidebar />
+                <Body />
             </div>
             
             {/* footer */}
             <div className='footer'>
-
+                <Footer />
             </div>
         </div>
     )
